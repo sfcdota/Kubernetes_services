60e7d6a4e8aa42a858e73b090879c140698ef1c3
sfcdota <sfcdota@gmail.com>
2021-01-12 18:14:46 +0300

    nginx metallb done

--- setup.sh
+++ setup.sh
@@ -3,11 +3,56 @@
 #                                                         :::      ::::::::    #
 #    setup.sh                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: cbach <cbach@student.21-school.ru>         +#+  +:+       +#+         #
+#    By: cbach <cbach@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/11 13:34:32 by cbach             #+#    #+#              #
-#    Updated: 2020/12/14 16:01:58 by cbach            ###   ########.fr        #
+#    Updated: 2021/01/12 17:52:29 by cbach            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+NGINX_DIR=srcs/nginx
+FTPS_DIR=srcs/ftps
+GRAFANA_DIR=srcs/grafana
+INFLUXDB_DIR=srcs/influxdb
+METALLB_DIR=srcs/metallb
+MYSQL_DIR=srcs/mysql
+PHPMYADMIN_DIR=srcs/phpmyadmin
+WORDPRESS_DIR=srcs/wordpress
+
+minikube start --vm-driver=virtualbox
+# clearing
+eval $(minikube docker-env)
+# kubectl delete daemonsets,replicasets,services,deployments,pods,rc --all
+# docker rm -f $(docker ps -a -q)
+# docker system prune -f
+# sleep 3
+
+
+# #metallb
+# kubectl delete -f srcs/nginx/nginx-service.yaml && minikube addons disable metallb && \
+# minikube addons enable metallb && kubectl apply -f srcs/metallb/metallb.yaml && kubectl create -f srcs/nginx/nginx-service.yaml
+#minikube addons enable metallb
+
+# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+# # On first install only
+# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+# kubectl apply -f srcs/metallb/metallb.yaml
+
+
+
+#images
+docker build -t nginx $NGINX_DIR
+
+
+#configs
+kubectl create -f $NGINX_DIR/nginx-deployment.yaml
+kubectl create -f $NGINX_DIR/nginx-service.yaml
+
+
+# to get into nginx service:
+# minikube tunnel
+# kubectl get svc
+# then go external-ip with or without some ports
 
 
--- srcs/nginx/srcs/boot.sh
+++ srcs/nginx/srcs/boot.sh
@@ -6,23 +6,16 @@
 #    By: cbach <cbach@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/11 13:34:02 by cbach             #+#    #+#              #
-#    Updated: 2020/12/11 13:34:35 by cbach            ###   ########.fr        #
+#    Updated: 2021/01/10 18:28:14 by cbach            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #NGINX
+# mv localhost /etc/nginx/conf.d/default.conf && mkdir -p /var/www/html && chown -R 777 /var/www/html/
 
-rm /etc/nginx/sites-available/default
-rm /etc/nginx/sites-enabled/default
+# openssl req -x509 -nodes -newkey rsa:2048 \
+# -keyout /etc/ssl/certs/localhost.key -out /etc/ssl/certs/localhost.pem \
+# -subj "/C=RU/ST=Kazan/L=Kazan/O=21School/OU=cbach/CN=localhost"
 
-mv localhost /etc/nginx/sites-available/
-ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/localhost
 
-chown -R www-data /var/www/html/*
-chmod -R 755 /var/www/html/*
-
-openssl req -x509 -nodes -newkey rsa:2048 \
--keyout /etc/ssl/certs/localhost.key -out /etc/ssl/certs/localhost.pem \
--subj "/C=RU/ST=Kazan/L=Kazan/O=21School/OU=cbach/CN=localhost"
-
-service nginx start
+bash
