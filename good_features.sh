c950677ab389c0e34ff7e3e7a3ddbfe9e0745042
sfcdota <sfcdota@gmail.com>
2021-01-14 00:46:50 +0300

    nginx with persistence behavior is completed (awa ssh connection)

---
+++ good_features.sh
@@ -0,0 +1,40 @@
+eval $(minikube docker-env)
+ssh-keygen -f "/home/sfcdota/.ssh/known_hosts" -R "192.168.99.114"
+
+docker rm -f $(docker ps -a -q) && docker build -t nginx srcs/nginx/ && kubectl rollout restart deployment
+
+kubectl exec -it $(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')     -- pkill nginx
+
+export IP=$(minikube ip)
+cub=$(echo $IP | grep -Eo "[0-9]+$")
+IP=$(echo $IP | sed -e "s/\.[0-9]\+$//")
+nmap -sP -PR $IP.$cub | grep "Host is up"
+while [ "$?" != 1 ]
+do
+    cub=$(($cub + 1))
+    nmap -sP -PR $IP.$cub | grep "Host is up"
+done
+IP="$IP.$cub"
+
+
+	Изменeния ip, портов и образа допускаются БЕЗ удаления предудщей сущности ( deploy, pod, svc) apply - f напишет вам "**" changed
+, значит ваши изменения применились. Если же выводит ошибку , значит точно удаляйте , и пробуйте добавить заново.
+	Для перезапуска metallb НЕ НУЖНО перезапускать миникуб. досаточно перезапустить саму metallb ( неожиданно, не правдали ? )
+addons disable && addons enable . Каждый раз после этого и КАЖДЫЙ РАЗ ПОСЛЕ СТАРТА миникуба нужно применять конфиг.
+	Иначе, даже если он вам показывает external ip  - по факту он работать не будет.
+Для небольших именений на уровне "а если так ?" не нужно пересобирать контейнер -
+залезьте внутрь kubectl exec -it *** sh и пропишите всё что вам нужно ручками ( для этого добавите нано или вим в сборку)
+	Даже если вы пересобрали докер образ, НЕ НУЖНО удалять deploy .
+Достаточно удалить под, или сделать kubectl rollout restart deployment
+и он подхватит новый образ сам ( для этого не забываем eval $(minikube docker-env)
+внутри каждой коносли в которой работаем. проверить к чему подключен докер проще всего docker ps
+
+
+docker run -dit -p 8080:80 -p 443:443 server && docker container ls
+
+docker run -dit -p 21:21 ftps && docker container ls
+
+kubectl exec -it [pod name] -- bash
+
+
+sudo docker inspect -f "{{ .NetworkSettings.IPAddress }}" nginx
--- setup.sh
+++ setup.sh
@@ -6,10 +6,11 @@
 #    By: cbach <cbach@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/11 13:34:32 by cbach             #+#    #+#              #
-#    Updated: 2021/01/12 17:52:29 by cbach            ###   ########.fr        #
+#    Updated: 2021/01/12 18:27:27 by cbach            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+#set DIR variables
 NGINX_DIR=srcs/nginx
 FTPS_DIR=srcs/ftps
 GRAFANA_DIR=srcs/grafana
@@ -19,7 +20,11 @@ MYSQL_DIR=srcs/mysql
 PHPMYADMIN_DIR=srcs/phpmyadmin
 WORDPRESS_DIR=srcs/wordpress
 
+
+#start minikube VM
 minikube start --vm-driver=virtualbox
+
+
 # clearing
 eval $(minikube docker-env)
 # kubectl delete daemonsets,replicasets,services,deployments,pods,rc --all
@@ -33,11 +38,8 @@ eval $(minikube docker-env)
 # minikube addons enable metallb && kubectl apply -f srcs/metallb/metallb.yaml && kubectl create -f srcs/nginx/nginx-service.yaml
 #minikube addons enable metallb
 
-# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-# # On first install only
-# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-# kubectl apply -f srcs/metallb/metallb.yaml
+#set metallb config
+kubectl apply -f srcs/metallb/metallb.yaml
 
 
 
