e875d38a3d3860d4ed93e0b77a16503a5c08195f
sfcdota <sfcdota@gmail.com>
2021-01-29 15:23:09 +0300

    added salts to wordpress config

--- get_ip.sh
+++
@@ -1,8 +0,0 @@
-lastoctet=$(echo $(minikube ip) | grep -Eo "[0-9]+$")
-lastoctet=$(($lastoctet + 1))
-freeipstart=$(echo $(minikube ip) | sed "s/[0-9]\{3,\}$/"$lastoctet"/g")
-freeipend=$(echo $(minikube ip) | sed "s/[0-9]\{3,\}$/"$(($lastoctet + 10))"/g")
-echo "minikube ip = $(minikube ip)\nFirst free ip in minikube subnet = $freeipstart"
-echo "changing metallb config to working properly with random minikube ip..."
-sed -i "s/IPRANGE/"$freeipstart"-"$freeipend"/g" srcs/metallb/metallb.yaml
-echo "iprange is now set"
